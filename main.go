f2f48eab1e27743efa2aa7a854ca47cddaa5c614
MrSpock1994 <willpokerpro2016@gmail.com>
2022-09-12 20:02:11 -0300

    Lops

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	var conference_name = "Go Conference"
 	const conference_tickets = 50
-	var remaining_tickets = 50
+	var remaining_tickets uint = 50
+	bookings := []string{}
 
 	//fmt.Println("Welcome to our", conference_name, "booking application!")
 	fmt.Printf("Welcome to our %v booking application.\n", conference_name)
@@ -13,14 +17,39 @@ func main() {
 	fmt.Printf("We have a total of %v tickets and %v are still avaiable.\n", conference_tickets, remaining_tickets)
 	fmt.Println("Get Your Tickets here!")
 
-	var first_name string
-	var user_tickets int
+	for {
+		var first_name string
+		var last_name string
+		var email string
+		var user_tickets uint
 
-	// Get user input
-	fmt.Println("Enter your first name: ")
-	fmt.Scan(&first_name)
+		// Get user input
+		fmt.Println("Enter your first name: ")
+		fmt.Scan(&first_name)
 
-	user_tickets = 2
-	fmt.Printf("User %v has %v tickets\n", first_name, user_tickets)
+		fmt.Println("Enter your last name: ")
+		fmt.Scan(&last_name)
+
+		fmt.Println("Enter your email: ")
+		fmt.Scan(&email)
+
+		fmt.Println("Enter number of tickets: ")
+		fmt.Scan(&user_tickets)
+
+		remaining_tickets -= user_tickets
+		bookings = append(bookings, first_name+" "+last_name)
+
+		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", first_name, last_name, user_tickets, email)
+		fmt.Printf("%v tickets remaining.\n", remaining_tickets)
+
+		firstnames := []string{}
+		for _, booking := range bookings {
+			var names = strings.Fields(booking)
+			firstnames = append(firstnames, names[0])
+		}
+
+		fmt.Printf("The first names of bookings are: %v\n", firstnames)
+
+	}
 
 }
